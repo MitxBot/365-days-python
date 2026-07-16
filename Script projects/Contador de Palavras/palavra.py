b526d77452cc5d40eb117eb0fa489d3b52c9c805
Vinicius <viniciusoduelista@gmail.com>
2026-07-16 16:14:43 -0300

    Adicionado a funcionalidade de escolher a pasta onde esta o arquivo .txt

--- Script projects/Contador de Palavras/palavra.py
+++ Script projects/Contador de Palavras/palavra.py
@@ -1,4 +1,5 @@
 import string
+from pathlib import Path
 
 def analisar_texto(texto):
     texto = texto.strip()
@@ -34,15 +35,21 @@ while True:
         analisar_texto(texto)
 
     elif opcao == "2":
-        nome_arquivo = input("Nome do arquivo: ")
+        caminho = Path(input("\nDigite o caminho do arquivo (.txt): ").strip())#Necessário colocar da seguinte forma: C:\Users\Usuario\Documents\Pasta\arquivo.txt
 
-        try:
-            with open(nome_arquivo,"r",encoding="utf-8") as arquivo:
-                texto = arquivo.read()
+        if not caminho.exists():
+            print("\nArquivo não encontrado!")
+
+        elif caminho.suffix.lower() != ".txt":
+            print("\nO arquivo precisa ser do tipo .txt")
+
+        else:
+            try:
+                texto = caminho.read_text(encoding="utf-8")
                 analisar_texto(texto)
 
-        except FileNotFoundError:
-            print("Arquivo não encontrado!")
+            except Exception as erro:
+                print(f"\nErro ao ler o arquivo: {erro}")
 
     elif opcao == "3":
         print("\nPrograma encerrado.")
