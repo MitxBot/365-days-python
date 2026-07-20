9598f92a0797f62b9da7ab9af95373c6e31e0aa8
Vinicius <viniciusoduelista@gmail.com>
2026-07-20 14:52:26 -0300

    Ajustes

--- Script projects/Conversor de Audio para Video/video.py
+++ Script projects/Conversor de Audio para Video/video.py
@@ -1,21 +1,21 @@
 from moviepy.editor import AudioFileClip, ImageClip
 
 def audio_para_video():
-    # Pega os caminhos a partir da entrada do usuário
+    #Pega os caminhos a partir da entrada do usuário.
     audio_path = input("Digite o caminho do arquivo de áudio: ")
     image_path = input("Digite o caminho da imagem: ")
     output_path = input("Digite o nome do arquivo de saída (ex: video.mp4): ")
 
-    # Carrega o áudio
+    #Carrega o áudio.
     audio = AudioFileClip(audio_path)
 
-    # Cria clipe de imagem com duração igual ao áudio
+    #Cria clipe de imagem com duração igual ao áudio.
     imagem = ImageClip(image_path).set_duration(audio.duration)
 
-    # Adiciona o áudio ao vídeo
+    #Adiciona o áudio ao vídeo.
     video = imagem.set_audio(audio)
 
-    # Exporta como mp4
+    #Exporta como mp4.
     video.write_videofile(output_path, fps=24)
 
 if __name__ == "__main__":
