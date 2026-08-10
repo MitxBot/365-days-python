57a9253a9576cc0bd87a2f379e63078ea665e15b
Vinicius <viniciusoduelista@gmail.com>
2026-08-10 16:02:33 -0300

    Novas funções

--- Game projects/Enciclopédia de Personagens/pedia.py
+++ Game projects/Enciclopédia de Personagens/pedia.py
@@ -22,3 +22,37 @@ def carregar():
             return json.load(arquivo)
 
     return []
+
+#Registrar
+
+def registrar(personagens):
+
+    print("\n===== Novo Personagem =====")
+
+    nome = input("Nome: ").strip()
+    obra = input("Obra: ").strip()
+    especie = input("Espécie: ").strip()
+    genero = input("Gênero: ").strip()
+    idade  = input("Idade: ").strip()
+    ocupacao = input("Ocupação: ").strip()
+    personalidade = input("Personalidade: ").strip()
+    habilidade = input("Habilidade: ").strip()
+    descricao = input("Descrição: ").strip()
+
+    personagem = {
+        "Nome": nome,
+        "Obra": obra,
+        "Espécie": especie,
+        "Gênero": genero,
+        "Idade": idade,
+        "Ocupação": ocupacao,
+        "Personalidade": personalidade,
+        "Habilidade": habilidade,
+        "Descrição": descricao
+    }
+
+    personagens.append(personagem)
+
+    salvar(personagens)
+
+    print("\nPersonagem cadastrado com sucesso!")
