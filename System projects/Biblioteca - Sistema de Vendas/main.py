117fcd81a0764989d3dca51da3cbd0038627cccf
Vinicius <viniciusoduelista@gmail.com>
2026-08-23 16:57:49 -0300

    Sistema de vendas para biblioteca

---
+++ System projects/Biblioteca - Sistema de Vendas/main.py
@@ -0,0 +1,13 @@
+from database import inicializar_banco
+from services import (
+    cadastrar_livro,
+    listar_livros,
+    cadastrar_cliente,
+    listar_clientes,
+    buscar_livro,
+    registrar_venda,
+    listar_vendas,
+    detalhes_venda
+)
+
+def ler_float(mensagem):
