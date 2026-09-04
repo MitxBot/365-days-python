e25084c11253aaa7cfa617952ffb4ed64e6694f8
Vinicius <viniciusoduelista@gmail.com>
2026-09-04 14:56:22 -0300

    Nova função adicionada

--- System projects/Biblioteca - Sistema de Vendas/services.py
+++ System projects/Biblioteca - Sistema de Vendas/services.py
@@ -9,9 +9,28 @@ def cadastrar_livro(titulo,autor,preco,estoque):
         """
         INSERT INTO livros (titulo, autor, preco, estoque)
         VALUES (?, ?, ?, ?)
-""",
-    (titulo, autor, preco, estoque)
-)
+        """,
+        (titulo, autor, preco, estoque)
+    )
 
     conexao.commit()
     conexao.close()
+
+def listar_livro():
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        SELECT id, titulo, autor, preco, estoque
+        FROM livros
+        ORDER BY titulo
+        """
+    )
+
+    livros = cursor.fetchall()
+
+    conexao.close()
+
+    return livros
