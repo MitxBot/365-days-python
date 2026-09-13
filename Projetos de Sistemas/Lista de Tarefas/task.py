c2b7f3f482ff54b8279a5c98615fb050b3fd4437
Vinicius <viniciusoduelista@gmail.com>
2026-09-13 15:30:54 -0300

    Função finalizada

--- Projetos de Sistemas/Lista de Tarefas/task.py
+++ Projetos de Sistemas/Lista de Tarefas/task.py
@@ -1,1 +1,16 @@
 tarefas = []
+
+def adicionar_tarefa():
+
+    tarefa = input("Digite uma tarefa: ").strip()
+
+    if tarefa:
+        tarefas.append({
+            "nome" : tarefa,
+            "concluida" : False
+        })
+
+        print("Tarefa adicionada!")
+
+    else:
+        print("A tarefa não pode estar vazia.")
