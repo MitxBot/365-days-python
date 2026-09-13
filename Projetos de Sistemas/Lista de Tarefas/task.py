a451c2fd02e0d9e6e217bab5075273293bc3d857
Vinicius <viniciusoduelista@gmail.com>
2026-09-13 15:36:24 -0300

    Função listar tarefa finalizada

--- Projetos de Sistemas/Lista de Tarefas/task.py
+++ Projetos de Sistemas/Lista de Tarefas/task.py
@@ -14,3 +14,16 @@ def adicionar_tarefa():
 
     else:
         print("A tarefa não pode estar vazia.")
+
+def listar_tarefa():
+
+    if not tarefas:
+        print("\nNenhuma tarefa cadastrada.")
+        return
+
+    print("===== Tarefas =====")
+
+    for i,tarefa in enumerate(tarefas,start=1):
+        status = "Completo" if tarefa["concluida"] else ""
+
+        print(f"{i}. [{status}] {tarefa['nome']}")
