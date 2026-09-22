7c74d8b28615c74cb397fcc56c295e708bdce5c6
Vinicius <viniciusoduelista@gmail.com>
2026-09-22 17:59:14 -0300

    Comentários adicionados

--- Projetos de Automação/Conversor de .txt/conv_txt.py
+++ Projetos de Automação/Conversor de .txt/conv_txt.py
@@ -2,11 +2,13 @@ from pathlib import Path
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 
-entrada = Path(r"")
-saida = Path(r"")
+entrada = Path(r"") #Define a pasta onde estão os arquivos .txt
+saida = Path(r"") #Define a pasta onde os PDFs convertidos serão salvos
 
+#Cria a pasta de saída caso ela ainda não exista e também cria as pastas que estiverem faltando no caminho
 saida.mkdir(parents=True,exist_ok=True)
 
+#Obtém alguns estilos de texto prontos do ReportLab
 styles = getSampleStyleSheet()
 
 for arquivo in entrada.glob("*.txt"):
