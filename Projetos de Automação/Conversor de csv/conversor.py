c98e63962205cdf2b08c86c6fc80ab1a6f74f5dd
Vinicius <viniciusoduelista@gmail.com>
2026-09-24 18:16:26 -0300

    Comentários adicionados

--- Projetos de Automação/Conversor de csv/conversor.py
+++ Projetos de Automação/Conversor de csv/conversor.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import pandas as pd
 
-PASTA_ENTRADA = Path(r"")
-PASTA_SAIDA = Path(r"")
+#Define a pasta onde estão os arquivos CSV
+PASTA_ENTRADA = Path(r"") #Coloque aqui o caminho da pasta de entrada
 
+#Define a pasta onde os arquivos Excel convertidos serão salvos
+PASTA_SAIDA = Path(r"") #Coloque aqui o caminho da pasta de saída
+
+#Cria a pasta de saída caso ela ainda não exista
 PASTA_SAIDA.mkdir(exist_ok=True)
 
 for arquivo in PASTA_ENTRADA.glob("*.csv"):
